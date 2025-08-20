3f6431dacb134222644b368e4b207cdb99e69062
Prateek Shukla <prateek.shukla@publicissapient.com>
2025-08-21 00:55:18 +0530

    removed .js and .map.js files from commiting and defined basic ts types

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
-let firstname = "Prateekk"
-console.log(firstname);
+let myName: string = "Prateek";
+let age: number = 27;
+let isMarried: boolean = false;
+let plans: any;
+let album: string | number;    // union
+let re: RegExp = /\w+/g        // regular expression
+
+const sum = (a: number,b: number) => {
+    return a + b
+}
